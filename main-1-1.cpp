6956b9e3d73821f3f689210c0b5d35f7353ca5b4
ngochan-ngo <161391723+ngochan-ngo@users.noreply.github.com>
2024-03-15 12:12:08 +1030

    Delete main-1-1.cpp

--- main-1-1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-extern sum_diagonal(int [4][4]);
-
-int main() {
-    int array[4][4] = {{0,1,2,3}, {4,5,6,7}, {8,9,0,1}, {2,3,4,5}};
-    std::cout << sum_diagonal(array);
-    return 0;
-}
